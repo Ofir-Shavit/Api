6a07b2d2f3259f43c6faa345c35935375534f211
Ofir Shavit <ofirshavit100@gmail.com>
2021-06-19 03:14:45 +0300

    Route to fetch profile

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,16 @@ const app = express();
 app.use(logger('dev'));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    const {query: {name}} = req;
-    res.send(`Good luck ${name || 'Ofir'}!`);
+app.get('/profile', (req, res) => {
+
+    const name = 'Ofir';
+    const imageUrl = 'https://lh3.googleusercontent.com/ogw/ADea4I5RiXp7B5h-rbB2ESdTxDs37KdJZwN9N-0nkTeNHfI=s192-c-mo';
+
+    res.json({name, imageUrl});
+
 });
 
-const port = 80;
+const port = 8080;
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}!`);
